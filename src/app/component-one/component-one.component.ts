fed1c9aef489495793e1bb0a889136e60067b3df
brich33713 <brich33713@gmail.com>
2021-07-20 14:54:29 -0400

    input update

--- src/app/component-one/component-one.component.ts
+++ src/app/component-one/component-one.component.ts
@@ -9,7 +9,20 @@ export class ComponentOneComponent implements OnInit {
 
   constructor() { }
 
+  username: string = "";
+  emptyString: boolean = true;
+
   ngOnInit(): void {
   }
+
+  changeInput(event: Event){
+    this.username = (<HTMLInputElement>event.target).value;
+    this.emptyString = this.username.length != 0 ? false : true;
+  }
+
+  buttonClick(){
+    this.username = "";
+  }
+
 
 }
